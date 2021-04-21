3d99469123bc82692ac706e812124aa5ac8e2fd2
Sumi Kim <ksm2920@gmail.com>
2021-04-21 10:51:25 +0200

    add animation

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+window.addEventListener('load', function() {
+    const tl = new gsap.timeline({ repeat: -1, repeatDelay: 3});
+
+    tl.to('#ball', { x: 18 }, { x: 0, ease: Bounce.easeOut, duration: 5 });
+    tl.fromTo('#masks', { y: 0 }, { y: -260, duration: 0.5 })
+    tl.fromTo('#bell-mask', { y: -150 }, { y: 0, ease: Bounce.easeOut, duration: 1 });
+    tl.set('#user-group', {visibility: 'visible'});
+    tl.fromTo('#confetti', { y: -150 }, { y:0, ease: Elastic.easeOut, duration: 1 });
+    tl.fromTo('#cloud, #moment, #download', {y: 200 }, { y: 0 });
+    // tl.fromTo('#download');
+}, false);
