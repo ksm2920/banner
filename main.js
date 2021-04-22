9c1457fa5ae8d80b45e47e15a3c1cf3cb72df240
Sumi Kim <ksm2920@gmail.com>
2021-04-22 11:33:28 +0200

    add opacity to confetti/ change button scale repeat number to 3

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
     tl.fromTo('#masks', 1, { y: 0 }, { y: -260})
     tl.fromTo('#bell-mask', 1,  { y: -150 }, { y: 0, ease: Bounce.easeOut});
     tl.to('#user-group', 1.5, {opacity: 1});
-    tl.fromTo('#confetti', 1.5, { y: -150 }, { y:0, ease: Elastic.easeOut});
+    tl.fromTo('#confetti', 1.5, { opacity: 0, y: -150 }, { opacity:1, y: 0, ease: Elastic.easeOut});
     tl.staggerFromTo('#cloud, #moment, #download', 2 , {opacity:0, y:90, ease: Elastic.easeOut.config(1.2, 0.5)}, {opacity:1, y:0, ease: Elastic.easeOut.config(1.2, 0.5)}, 0.03);
-    tl.fromTo('#download', 0.3, { scale: 1}, { scale: 1.2, repeat: 4, ease: Power0.easeOut, yoyo:true});
+    tl.fromTo('#download', 0.3, { scale: 1}, { scale: 1.2, repeat: 3, ease: Power0.easeOut, yoyo:true});
 }, false);
